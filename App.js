c6986052123c3564f162a4ec7994a2f8ef172fbd
Anush-TFT <sharma.anush@tftus.com>
2023-08-08 16:50:51 +0530

    Namastey React

--- App.js
+++ App.js
@@ -1,8 +1,33 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+
+// React Element
 const heading = React.createElement('h1' , 
 {
     id : 'heading' 
-} , 'Hello World ! with React');
-console.log(heading) 
+} , 'Hello World ! with React'); 
 // it nothing buts returns object
+console.log(heading)
+
+
+const Title = ()=>{
+    return (
+        <h1>Bro React</h1>
+    )
+}
+
+// React Functinal component 
+const HeadingComponent = ()=>{
+    return (
+        <>
+          <Title/>
+         <h1>Functional Component Lets Rocks</h1>
+        </>
+      
+    )
+}
+
+// JSX 
+ // const jsxElement = <h1>Hello React !</h1>
  const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+root.render( <HeadingComponent/>);
---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,9 @@
+const heading = React.createElement("h1", {
+    id: "heading"
+}, "Hello World ! with React");
+console.log(heading);
+// it nothing buts returns object
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
